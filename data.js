aad89530610971bf406c7899b146c2167e66757f
Nisarga Patel <np8@umbc.edu>
2015-12-25 18:05:58 -0500

    added nested callbacks so articles always load

--- data.js
+++ data.js
@@ -1,53 +1,92 @@
-var app = angular.module('myApp', []).controller('HackerNewsCtrl', function($scope, $http) {
-
-            $scope.hackerNews = [];
-            
-            $http.jsonp('https://www.kimonolabs.com/api/3ohsojdc?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
+var app = angular.module('myApp', []).factory('newsHandler', function($http){
+    
+    
+    var hackerNews = function(){
+        return $http.jsonp('https://www.kimonolabs.com/api/3ohsojdc?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
                for(var i = 0; i < data.data.results.all_news.length; i++){
                    data.data.results.all_news[i].src = "Hacker News";
                }
-               $scope.hackerNews = data.data.results.all_news;
+               return data.data.results.all_news;
             });
-            
-            $http.jsonp('https://www.kimonolabs.com/api/d2859ucg?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
-                
+    };
+
+    var googleNews = function(){
+        return $http.jsonp('https://www.kimonolabs.com/api/d2859ucg?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
+
                for(var i = 0; i < data.data.results.all_news.length; i++){
                    data.data.results.all_news[i].src = "Google News";
-                   $scope.hackerNews.push(data.data.results.all_news[i])
                }
+                return data.data.results.all_news;
             });
-            
-            $http.jsonp('https://www.kimonolabs.com/api/3jmix9t2?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
-                
+    };
+
+    var lifeHacker = function(){
+        return $http.jsonp('https://www.kimonolabs.com/api/3jmix9t2?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
+
                for(var i = 0; i < data.data.results.all_news.length; i++){
                    data.data.results.all_news[i].src = "Lifehacker";
-                   $scope.hackerNews.push(data.data.results.all_news[i])
                }
+                return data.data.results.all_news;
             });
-            
-            $http.jsonp('https://www.kimonolabs.com/api/8mt4om9w?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
-                
+    };
+
+    var gizmodo = function(){
+        return $http.jsonp('https://www.kimonolabs.com/api/8mt4om9w?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
+
                for(var i = 0; i < data.data.results.all_news.length; i++){
                    data.data.results.all_news[i].src = "Gizmodo";
-                   $scope.hackerNews.push(data.data.results.all_news[i])
                }
+                return data.data.results.all_news;
             });
-            
-            $http.jsonp('https://www.kimonolabs.com/api/7j1v7o3i?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
-                
+    };
+
+    var cnnTech = function(){
+        return $http.jsonp('https://www.kimonolabs.com/api/7j1v7o3i?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
+
                for(var i = 0; i < data.data.results.all_news.length; i++){
                    data.data.results.all_news[i].src = "CNN Tech";
-                   $scope.hackerNews.push(data.data.results.all_news[i])
                }
+                return data.data.results.all_news;
             });
-            
-            $http.jsonp('https://www.kimonolabs.com/api/akrnd8po?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
-                
+    };
+
+    var arsTechnica = function(){
+        return $http.jsonp('https://www.kimonolabs.com/api/akrnd8po?apikey=DVJ9Ockxnux8SrQBtOKvVYMcmCd2ChQ3&callback=JSON_CALLBACK').then(function(data){
+
                for(var i = 0; i < data.data.results.all_news.length; i++){
                    data.data.results.all_news[i].src = "Ars Technica";
-                   $scope.hackerNews.push(data.data.results.all_news[i])
                }
+                return data.data.results.all_news;
             });
-            
-            $scope.hlen = $scope.hackerNews.length;
+    };
+
+
+    //hackerNews.concat(googleNews, lifeHacker, gizmodo, cnnTech, arsTechnica);
+    var functions = [hackerNews, googleNews, lifeHacker, gizmodo, cnnTech, arsTechnica];
+    return {functions: functions};
+    
+    
+}).controller('HackerNewsCtrl', function($scope, $http, newsHandler) {
+
+            $scope.hackerNews = [];
+            var articles = newsHandler.functions;
+            // riding the callback rollercoaster
+            articles[0]().then(function(result){
+                $scope.hackerNews = result;
+                articles[1]().then(function(result){
+                    $scope.hackerNews = $scope.hackerNews.concat(result);
+                    articles[2]().then(function(result){
+                        $scope.hackerNews = $scope.hackerNews.concat(result);
+                        articles[3]().then(function(result){
+                            $scope.hackerNews = $scope.hackerNews.concat(result);
+                            articles[4]().then(function(result){
+                                $scope.hackerNews = $scope.hackerNews.concat(result);
+                                articles[5]().then(function(result){
+                                    $scope.hackerNews = $scope.hackerNews.concat(result);
+                                });
+                            });
+                        });
+                    });
+                });  
+            }); 
         });
